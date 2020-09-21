2a2c2b9fa3f3e606873640e457c8f21d92b7eaee
v5al <udamvisal18@kit.edu.kh>
2020-09-21 21:48:48 +0700

    to-do app

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav>
+        <div class="nav-wrapper">
+            <div class="container">
+                <a href="#" class="brand-logo">To Do App</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li>
+                        <router-link to="/">Home</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/about">About</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style>
+
+</style>
